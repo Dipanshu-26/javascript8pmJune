aa44e2ef95a7b87f3eeffc6f472a69c797b205a2
Dipanshu-26 <dnchawde@gmail.com>
2026-07-23 20:20:44 +0530

    23-june

---
+++ 2-day2.js
@@ -0,0 +1,77 @@
+console.log("javascript")
+
+//let const
+let a=10
+console.log(a)
+console.log(typeof(a))
+
+//let a=20   //SyntaxError: Identifier 'a' has already been declared
+
+a=20
+console.log(a)
+
+const r=66
+console.log(r)
+
+//r=77    //TypeError: Assignment to constant variable
+
+a=10.5
+console.log(a)
+console.log(typeof(a))
+
+a=-10
+console.log(a)
+console.log(typeof(a))
+
+a=-10.5
+console.log(a)
+console.log(typeof(a))
+
+a="dipanshu"
+console.log(a)
+console.log(typeof(a))
+
+a='10'
+console.log(a)
+console.log(typeof(a))
+
+a="@#$"
+console.log(a)
+console.log(typeof(a))
+
+a="dip@123"
+console.log(a)
+console.log(typeof(a))
+
+a=true
+console.log(a)
+console.log(typeof(a))
+
+a=false
+console.log(a)
+console.log(typeof(a))
+
+let True = 500
+a=True
+console.log(a)
+console.log(typeof(a))
+
+let b = a 
+console.log(b)
+
+a = "True"
+console.log(a)
+console.log(typeof(a))
+
+a="true"
+console.log(a)
+console.log(typeof(a))
+
+let x=10
+let y="10"
+
+console.log(x)
+console.log(typeof(x))
+
+console.log(y)
+console.log(typeof(y))
