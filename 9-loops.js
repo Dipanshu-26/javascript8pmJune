c0c09f1b6cda9cb583929eefbf65953c32adcd1c
Dipanshu-26 <dnchawde@gmail.com>
2026-08-03 20:28:58 +0530

    03Aug

---
+++ 9-loops.js
@@ -0,0 +1,64 @@
+//for , while 
+//for 
+
+//syntax 
+//for(inilization;condition;inc/dec){...}
+
+//print hello 5 times 
+
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+
+for(let i=1;i<=5;i++){        //i++ ==> i=i+1
+    console.log("hello")
+}
+
+//print 1 to 10 
+
+for(let i=1;i<=10;i++){       //i=1,2,3,4,5,6,7,8,9,10,11
+    console.log(i)            //1,2,3,4,5,6,7,8,9,10
+}
+
+//print 0 to 10 
+for(let i =0;i<=10;i++){
+    console.log(i)
+}
+
+//print table of 2
+//2,4,6,8,10.....20
+
+for(let a=2;a<=20;a=a+2){
+    console.log(a)
+}
+
+//-------------------------------------
+for(let i =1;i<=10;i++){
+    console.log(i*2)
+}
+//------------------------------------------
+
+//print table of 5
+for(let i=5;i<=50;i=i+5){
+    console.log(i)
+}
+//----------------------------------------
+console.log("-----------")
+//print 1 to 10 inreverse
+for(let i=10;i>=1;i--){        //i-- ==>i=i-1
+    console.log(i)
+}
+
+console.log("-----------")
+//--------------------------------
+for(i=20;i>=2;i=i-2){    //20,18,16.......2,0
+    console.log(i)       //20,18,16.......2
+}
+console.log("----------------")
+for(let i=50;i>=5;i=i-5){
+    console.log(i)
+}
+//--------------------------------
+//while loops
